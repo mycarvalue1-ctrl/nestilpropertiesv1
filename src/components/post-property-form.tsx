1811525948fb27c60557b4769a6760e4fd9176cb
Mycarvalue <mycarvalue1@gmail.com>
2026-03-01 05:15:26 +0000

    Try fixing this error: `Runtime ReferenceError: useMemo is not defined.

--- src/components/post-property-form.tsx
+++ src/components/post-property-form.tsx
@@ -1,7 +1,7 @@
 
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useMemo } from 'react';
 import Image from 'next/image';
 import { useForm, useWatch } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
