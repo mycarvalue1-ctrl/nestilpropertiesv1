0460e9b8fc96597b79ed1f7134b71456715c245f
Mycarvalue <mycarvalue1@gmail.com>
2026-03-09 04:42:34 +0000

    the scrolling is very slow can make some normal speed  (_for element <Ti

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -105,7 +105,7 @@ export default {
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-        'ticker': 'ticker 60s linear infinite',
+        'ticker': 'ticker 40s linear infinite',
         'blob': 'blob 8s ease-in-out infinite',
       },
     },
